372dff4f1c76db8586fe14c628de718b2c19c690
R.I.Pienaar <rip@devco.net>
2010-05-23 22:28:23 +0100

    new fact

---
+++ cluster/cluster.rb
@@ -0,0 +1,18 @@
+if Facter.hostname =~ /(\d+)$/
+    hostname = $1.to_i
+
+    # set this to the amount of clusters
+    # you'd like to create
+    clusters = 3
+
+    NATOALPHA = ["alfa", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel",
+                 "india", "juliet", "kilo", "lima", "mike", "november", "oscar", "papa",
+                 "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey",
+                 "xray", "yankee", "zulu"]
+
+	cluster = NATOALPHA[hostname % clusters]
+
+	Facter.add(:cluster) do
+		setcode { cluster }
+	end
+end
