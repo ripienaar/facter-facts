82dc8ad8994b34d06ff1ff5ce442449b95957b6c
R.I.Pienaar <rip@devco.net>
2010-07-03 17:48:08 +0100

    Fix facter variable type madness

--- puppet-enabled/puppet_enabled.rb
+++ puppet-enabled/puppet_enabled.rb
@@ -5,11 +5,11 @@ Facter.add(:puppet_enabled) do
         # if the lock file exist and has no PID in it, then puppetd is
         # disabled, if there's a PID then its running or dead unexpectedly
         if File::Stat.new(lockfile).zero?
-            setcode { 0 }
+            setcode { "0" }
         else
-            setcode { 1 }
+            setcode { "1" }
         end
     else
-        setcode { 1 }
+        setcode { "1" }
     end
 end
