54cbac53f57a213a940a95b1b5577c731cad69ac
R.I.Pienaar <rip@devco.net>
2010-07-03 15:43:05 +0100

    Add puppet_enabled fact

---
+++ puppet-enabled/puppet_enabled.rb
@@ -0,0 +1,15 @@
+Facter.add(:puppet_enabled) do
+    lockfile = "/var/lib/puppet/state/puppetdlock"
+
+    if File.exists?(lockfile)
+        # if the lock file exist and has no PID in it, then puppetd is
+        # disabled, if there's a PID then its running or dead unexpectedly
+        if File::Stat.new(lockfile).zero?
+            setcode { 0 }
+        else
+            setcode { 1 }
+        end
+    else
+        setcode { 1 }
+    end
+end
